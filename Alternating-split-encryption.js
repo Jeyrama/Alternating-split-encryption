78eb2d5fac42c33bfefc7c88195009ad031e10f1
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-01 14:04:39 -0700

    Update Alternating-split-encryption.js

--- Alternating-split-encryption.js
+++ Alternating-split-encryption.js
@@ -22,3 +22,19 @@ return the first argument without changes.
 
 
 // Solution
+
+function encrypt(text, n) {
+  console.log(text, n);
+  if (!text || n <= 0) return text;
+  while (n--) {
+    let ans = '';
+    for (let i = 1; i < text.length; i += 2) {
+      ans += text[i];
+    }
+    for (let i = 0; i < text.length; i += 2) {
+      ans += text[i];
+    }
+    text = ans;
+  }
+  return text;
+}
